c0f663056ce3ffead64dcd8059ac00db928932a9
PUMIN-Dev <81092747+PUMIN-Dev@users.noreply.github.com>
2021-03-24 22:08:42 +0700

    mainExercise4

---
+++ Exercise4_Pumin_L.py
@@ -0,0 +1,9 @@
+EngScore=82.2
+GenBusinessScore=92.63
+IntroComScore=86.98
+ComPorScore=99.99
+print("--- Your Score ---")
+print("Foundation English :",EngScore)
+print("General Business :",GenBusinessScore)
+print("Introduction to Computer Systems :",IntroComScore)
+print("Computer Programming :"            ,ComPorScore)
