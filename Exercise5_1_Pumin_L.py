d5a0b22323ecdcd66b87603ca09055dfa23f76bc
PUMIN-Dev <81092747+PUMIN-Dev@users.noreply.github.com>
2021-03-25 21:25:58 +0700

    mineExercise5_1_

---
+++ Exercise5_1_Pumin_L.py
@@ -0,0 +1,11 @@
+a = int(input("First Number : "))
+b = int(input("Second Number : "))
+print(a,"+",b,"=",a+b)
+print(a,"-",b,"=",a-b)
+print(a,"*",b,"=",a*b)
+print (a,"/",b,"=",int(a/b))
+
+
+
+
+
